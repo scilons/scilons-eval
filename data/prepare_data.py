504092c68c547c5b38aa72e1c21808c8ea4f4d6e
ryabhmd <77779090+ryabhmd@users.noreply.github.com>
2024-05-03 12:11:14 +0200

    changed imports in prepare_data.py

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from datasets import Dataset
-from data_prep_utils import read_txt_file_ner, tokenize_data_ner, prepare_input_ner, extract_labels
+from data.data_prep_utils import read_txt_file_ner, tokenize_data_ner, prepare_input_ner, extract_labels
 
 
 class DatasetPrep:
