5e3df6cbbc7d387d516acd6fad1b09859077b7c2
Raia Abu Ahmad <raya.abu.ahmad@gmail.com>
2024-05-16 14:03:07 +0200

    updated HfArgumentParser usage

--- model_eval.py
+++ model_eval.py
@@ -4,6 +4,7 @@ from transformers import (
     AutoModelForSequenceClassification,
     Trainer,
     TrainingArguments,
+    HfArgumentParser
 )
 from data.prepare_data import DatasetPrep
 from data.data_prep_utils import extract_spans
@@ -12,6 +13,8 @@ from collections import defaultdict
 import argparse
 import torch
 import os
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 class ModelEval:
@@ -51,11 +54,12 @@ class ModelEval:
             save_steps=self.hf_args.save_steps,
             evaluation_strategy=self.hf_args.evaluation_strategy,
             eval_steps=self.hf_args.eval_steps,
+            learning_rate=self.hf_args.learning_rate
         )
 
         trainer = Trainer(
             model=model,
-            args=training_args,
+            args=self.hf_args,
             train_dataset=dataset_dict["train"],
             eval_dataset=dataset_dict["dev"],
         )
@@ -168,33 +172,53 @@ class ModelEval:
         elif self.task == 'dep':
             return 0
 
+@dataclass
+class CustomArguments:
+    task: str = field(
+        metadata={"help": "Specify the task name"}
+    )
+    model: str = field(
+        metadata={"help": "Specify the model name from huggingface"}
+    )
+    tokenizer: str = field(
+        metadata={"help": "Specify the tokenizer name from huggingface"}
+    )
+    data: str = field(
+        metadata={"help": "Specify the data path (the folder that contains train.txt, dev.txt, and test.txt)"}
+    )
 
 def main():
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--task", type=str, required=True, help="Specify the task name")
     parser.add_argument("-m", "--model", type=str, required=True, help="Specify the model name from huggingface")
     parser.add_argument("-k", "--tokenizer", type=str, required=True, help="Specify the tokenizer name from huggingface")
     parser.add_argument("-d", "--data", type=str, required=True, help="Specify the data path (the folder that contains train.txt, dev.txt, and test.txt)")
-
-    args = parser.parse_args()
-
-    task = args.task
-    model_name = args.model
-    tokenizer_name = args.tokenizer
-    data_path = args.data
-
-    hf_parser = argparse.ArgumentParser(description="Hugging Face Transformers Training Script")
-    TrainingArguments.add_argparse_args(parser)
     # Add TrainingArguments with custom defaults
-    parser.add_argument('--output_dir', type=str, default='./results', help='The output directory where the model predictions and checkpoints will be written.')
+    parser.add_argument('--output_dir', type=str, default='./results',
+                        help='The output directory where the model predictions and checkpoints will be written.')
     parser.add_argument('--num_train_epochs', type=int, default=3, help='Total number of training epochs to perform.')
-    parser.add_argument('--per_device_train_batch_size', type=int, default=8, help='Batch size per GPU/TPU core/CPU for training.')
+    parser.add_argument('--per_device_train_batch_size', type=int, default=8,
+                        help='Batch size per GPU/TPU core/CPU for training.')
+    parser.add_argument('--report_to', type=str, default='wandb')
     parser.add_argument('--save_steps', type=int, default=500, help='Save checkpoint every X updates steps.')
     parser.add_argument('--logging_dir', type=str, default='./logs', help='Tensorboard log directory.')
     parser.add_argument('--logging_steps', type=int, default=500, help='Log every X updates steps.')
-    parser.add_argument('--evaluation_strategy', type=str, default='steps', choices=['no', 'steps', 'epoch'], help='Evaluation strategy to use.')
+    parser.add_argument('--evaluation_strategy', type=str, default='steps', choices=['no', 'steps', 'epoch'],
+                        help='Evaluation strategy to use.')
     parser.add_argument('--eval_steps', type=int, default=500, help='Evaluate on dev set every X steps.')
-    hf_args = hf_parser.parse_args()
+    parser.add_argument('--learning_rate', type=float, default=2e-5, help='Evaluate on dev set every X steps.')
+    """
+
+    parser = HfArgumentParser((TrainingArguments, CustomArguments))
+    training_args, custom_args = parser.parse_args_into_dataclasses()
+
+    #args = parser.parse_args()
+
+    task = custom_args.task
+    model_name = custom_args.model
+    tokenizer_name = custom_args.tokenizer
+    data_path = custom_args.data
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -204,7 +228,7 @@ def main():
         tokenizer_name=tokenizer_name,
         data_path=data_path,
         device=device,
-        hf_args = hf_args
+        hf_args = training_args
     )
 
     model_eval.evaluate_model()
